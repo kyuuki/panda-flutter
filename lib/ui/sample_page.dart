4ef6daf4fc93950e6580f6b5cacb202e504bddab
kyuuki <kyuuki.japan@gmail.com>
2022-06-25 16:10:13 +0900

    Change the sample page

--- lib/ui/sample_page.dart
+++ lib/ui/sample_page.dart
@@ -6,19 +6,52 @@ class SamplePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("sample_base")),
+      appBar: AppBar(title: const Text("パンダ")),
+      persistentFooterButtons: [
+        Center(
+          child: ElevatedButton(
+            child: const Text("画像ボタン"),
+            onPressed: () async {
+            },
+          ),
+        ),
+      ],
       body: Column(
         children: [
           Padding(
             padding: const EdgeInsets.all(16.0),
+            child: Image.network("https://image.itmedia.co.jp/nl/articles/2002/22/nt2020_pandanuma_ep05a_w490.jpg")
+          ),
+          Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.network("https://www.nikkansports.com/general/nikkan/news/img/202111160000415-w500_1.jpg")
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
             child: Row(
               children: const [
                 Padding(
                   padding: EdgeInsets.all(10.0),
-                  child: Icon(Icons.vaccines),
+                  child: Icon(Icons.bed),
                 ),
                 Text(
-                  "Sample",
+                  "シャオシャオ",
+                  textAlign: TextAlign.right,
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Row(
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Icon(Icons.time_to_leave),
+                ),
+                Text(
+                  "レイレイ",
                   textAlign: TextAlign.right,
                   style: TextStyle(fontSize: 16.0),
                 ),
