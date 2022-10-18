98d21a56d74a5c2231e215c17eab7ea371b626b3
kyuuki <kyuuki.japan@gmail.com>
2022-10-18 19:06:56 +0900

    Modify package name from test code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sample_base/main.dart';
+import 'package:panda/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
